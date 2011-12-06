db413fecdfbc865c0691ef782f364d0727291c76
A. Murat Eren <a.murat.eren@gmail.com>
2011-12-06 00:02:14 -0500

    http://i.imgur.com/NrS1W.png is an example output of that little function that will be utilized to report basic stats about a fasta file..

--- pipeline/utils/fastalib.py
+++ pipeline/utils/fastalib.py
@@ -10,12 +10,24 @@
 #
 # Please read the docs/COPYING file.
 
+import os
+import sys
+import numpy
+
+sys.path.append('/'.join(os.path.abspath(__file__).split('/')[:-3]))
+try:
+    import pipeline.utils.utils
+    pp = pipeline.utils.utils.pp
+except:
+    pp = lambda x: x
+
 class SequenceSource:
-    def __init__(self, f_name, lazy_init = True):
+    def __init__(self, fasta_file_path, lazy_init = True):
+        self.fasta_file_path = fasta_file_path
         self.pos = 0
         self.id  = None
         self.seq = None
-        self.file_pointer = open(f_name)
+        self.file_pointer = open(self.fasta_file_path)
         self.file_pointer.seek(0)
         
         if lazy_init:
@@ -51,3 +63,90 @@ class SequenceSource:
         self.seq = None
         self.file_pointer.seek(0)
 
+    def visualize_sequence_length_distribution(self, title, dest = None, max_seq_len = None, xtickstep = None, ytickstep = None):
+        import matplotlib.pyplot as plt
+        import matplotlib.gridspec as gridspec
+
+        sequence_lengths = []
+    
+        self.reset()
+    
+        while self.next():
+            if self.pos % 10000 == 0 or self.pos == 1:
+                sys.stderr.write('\r[fastalib] Reading: %s' % (pp(self.pos)))
+                sys.stderr.flush()
+            sequence_lengths.append(len(self.seq))
+        
+        self.reset()
+    
+        sys.stderr.write('\n')
+    
+        if not max_seq_len:
+            max_seq_len = max(sequence_lengths) + (int(max(sequence_lengths) / 100.0) or 10)
+    
+        seq_len_distribution = [0] * (max_seq_len + 1)
+    
+        for l in sequence_lengths:
+            seq_len_distribution[l] += 1
+    
+        fig = plt.figure(figsize = (16, 12))
+        plt.rcParams.update({'axes.linewidth' : 0.9})
+        plt.rc('grid', color='0.50', linestyle='-', linewidth=0.1)
+    
+        gs = gridspec.GridSpec(10, 1)
+    
+        ax1 = plt.subplot(gs[0:8])
+        plt.grid(True)
+        plt.subplots_adjust(left=0.05, bottom = 0.03, top = 0.95, right = 0.98)
+    
+        plt.plot(seq_len_distribution, color = 'black', alpha = 0.3)
+        plt.fill_between(range(0, max_seq_len + 1), seq_len_distribution, y2 = 0, color = 'black', alpha = 0.15)
+        plt.ylabel('number of sequences')
+        plt.xlabel('sequence length')
+    
+        if xtickstep == None:
+            xtickstep = (max_seq_len / 50) or 1
+    
+        if ytickstep == None:
+            ytickstep = max(seq_len_distribution) / 20 or 1
+    
+        plt.xticks(range(xtickstep, max_seq_len + 1, xtickstep), rotation=90, size='xx-small')
+        plt.yticks(range(0, max(seq_len_distribution) + 1, ytickstep),
+                   [pp(y) for y in range(0, max(seq_len_distribution) + 1, ytickstep)],
+                   size='xx-small')
+        plt.xlim(xmin = 0, xmax = max_seq_len)
+        plt.ylim(ymin = 0, ymax = max(seq_len_distribution) + (max(seq_len_distribution) / 20.0))
+    
+        plt.figtext(0.5, 0.96, '%s' % (title), weight = 'black', size = 'xx-large', ha = 'center')
+    
+        ax1 = plt.subplot(gs[9])
+        plt.rcParams.update({'axes.edgecolor' : 20})
+        plt.grid(False)
+        plt.yticks([])
+        plt.xticks([])
+        plt.text(0.02, 0.5, 'total: %s / mean: %.2f / std: %.2f / min: %s / max: %s'\
+            % (pp(len(sequence_lengths)),
+               numpy.mean(sequence_lengths), numpy.std(sequence_lengths),\
+               pp(min(sequence_lengths)),\
+               pp(max(sequence_lengths))),\
+            va = 'center', alpha = 0.8, size = 'x-large')
+    
+        if dest == None:
+            dest = self.fasta_file_path
+
+        try:
+            plt.savefig(dest + '.tiff')
+        except:
+            plt.savefig(dest + '.png')
+    
+        try:
+            plt.show()
+        except:
+            pass
+    
+        return
+    
+
+if __name__ == '__main__':
+    fasta = SequenceSource(sys.argv[1])
+    fasta.visualize_sequence_length_distribution(title = sys.argv[2] if len(sys.argv) == 3 else 'None')
